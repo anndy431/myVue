ae449c70a01539137d208208e115a0f6a65c7fd6
zhouweihua <zhouweihua431@163.com>
2017-11-10 17:33:56 +0800

    myVue

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './src/main.js',//值可以是字符串、数组或对象
+    output: {
+        path: path.resolve(__dirname, './dist'),//Webpack结果存储
+        publicPath: '/dist/',//懵懂，懵逼，//然而“publicPath”项则被许多Webpack的插件用于在生产模式和开发模式下下更新内嵌到css、html，img文件里的url值
+        filename: 'build.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+            //自己加的
+            ,
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+            ,
+            {
+                test: /\.scss$/,
+                loader: "style-loader!css-loader!sass-loader!"
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {//webpack-dev-server配置
+        historyApiFallback: true,//不跳转
+        noInfo: true,
+        inline: true//实时刷新
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
+
+
+//webpack                       打包项目代码
+//webpack-dev-server            运行项目
+//git status                    查看状态
+//git add .                     添加变更的文件
+//git commit -m ""              起格名字提交到缓冲区
+//git commit log                查看日志
+//git pull origin 分支          拉分支代码
+//git push origin 分支          提交分支代码
+//git merge 分支                合并分支代码
+//git checkout 分支             切换分支
+//git checkout -b 分支名        创建并切换到新建的分支下 = git branch 分支名 + git checkout 分支名
